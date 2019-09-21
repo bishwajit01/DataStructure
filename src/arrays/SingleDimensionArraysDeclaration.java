3d828e7f7aa5a8ec431870bbfcad50ba8594a77e
bishwajit01 <bishwajit.vikram@gmail.com>
2019-09-21 15:56:16 +0530

    Added Remarks

--- src/arrays/SingleDimensionArraysDeclaration.java
+++ src/arrays/SingleDimensionArraysDeclaration.java
@@ -15,10 +15,13 @@ public class SingleDimensionArraysDeclaration {
     public static void main(String[] srgs) {
         int a[] = new int[5];
         int[] b = new int[5];
+        
+        //Initializing array with 
         for (int i = 0; i < a.length; i++) {
             a[i] = i + 1;
             b[i] = (i + 1) * (i + 1);
         }
+        
         //Initializing with the array index
         int[] c = { a[0] + 1 , 8 , 27 , 64 , (int) Math.pow(a[4] , 3) };
         
@@ -26,11 +29,13 @@ public class SingleDimensionArraysDeclaration {
             System.out.println(a[i] + " \t " + b[i] + " \t " + c[i]);
         }
         
+        // 
         int n = 5;
         int[] d = new int[n];
         for (int i = 0; i < n; i++) {
             d[i] = i;
         }
+        
         
         for (int e : d)
             System.out.print(e + " ");
